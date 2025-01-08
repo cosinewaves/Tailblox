4d52342ddf80ecdca7df97803850a6dc02ccf8b8
cosinewaves <158834632+cosinewaves@users.noreply.github.com>
2025-01-08 20:51:41 +0000

    Create init.lua

---
+++ src/init.lua
@@ -0,0 +1,10 @@
+--!strict
+local SchemeHandler = require(script.SchemeHandler)
+local EventListener = require(script.EventListener)
+
+local Tailblox = {
+	Scheme = SchemeHandler,
+	EventListener = EventListener,
+}
+
+return Tailblox
